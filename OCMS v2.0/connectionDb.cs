da94a92e9806824ec41e93af74d02788f9b3fea8
MuazAhmad <muazahmad24@yahoo.com>
2016-03-02 10:47:17 +0500

    my commit

---
+++ OCMS v2.0/connectionDb.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OCMS_v2._0
+{
+    class connectionDb
+    {
+        public MySqlConnection myConn;
+        public MySqlCommand SelectCommand;
+        public void connectionOpen()
+        {
+            try {
+                MySqlConnection myConn = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=''");
+                myConn.Open();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            
+        }
+        public void connectionClose()
+        {
+            myConn.Close();
+        }
+    }
+}
